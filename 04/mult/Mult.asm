d18667fadf4ef78db12751b6cc168e19e9dd2183
Charles Werk <charles.werk@umconnect.umt.edu>
2023-02-17 08:37:49 -0700

    mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,42 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Jump into the first STEP if R0 > 0.
+
+@R2	//GO TO FINAL ANSWER BOX
+M=0	//ZERO ANS BOX
+
+@R0
+D=M
+@END
+D;JEQ	//IF ONE PRODUCT IS ZERO
+
+@R1
+D=M
+@END
+D;JEQ	//IF ONE PRODUCT IS ZERO
+
+@R0	
+D=M	
+@R3	
+M=D	
+
+
+(LOOP)
+@R1	//GET 2ND NUM
+D=M	//D HAS 2ND NUM
+
+@R2	//GO TO FINAL ANSWER BOX
+M=D+M	//RAM[2] NOW HAS 2ND NUMBER + ITS PREVIOUS VALUE
+
+@R3	//GET 1ST NUM
+M=M-1	//1ST NUM-1
+
+D=M	
+@LOOP	//WHERE TO JUMP TO
+D;JGT	//JUMP		   
+
+
+(END)
+@END
+0;JMP	//FOREVER LOOP
