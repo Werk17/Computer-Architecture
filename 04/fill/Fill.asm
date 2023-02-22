33567dc9a9f7a0da1fd9146ee6cbbe24b8328bb2
Charles Werk <charles.werk@umconnect.umt.edu>
2023-02-21 20:49:18 -0700

    completed project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,45 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@color
+M=0 // Declare color, initialized to white '0'
+
+(LOOP)
+
+    @SCREEN //Assign screen range
+    D=A
+    @pixels
+    M=D
+
+    @KBD // Assign keyboard address
+    D=M
+    @BLACKEN
+    D;JGT // If greater than zero, make black (loop)
+
+    @color  //assign to white if not already
+    M=0 
+    @COLORIZE
+    0;JMP // color screen, unconditional jump
+
+    (BLACKEN) // assign color to black
+        @color
+        M=-1 // 2's compliment for contant 1's
+
+    (COLORIZE)
+        @color
+        D=M 
+        @pixels
+        A=M // pointer to start of pixel range, address to another address
+        M=D // Colorize these pixels
+
+        @pixels
+        M=M+1 // Increment pixels by one until 
+        D=M //    
+
+        @24576  // Here, we decrement from the end of the screen to the 
+        D=D-A   // beginning of the screen range until difference is less than 0
+        @COLORIZE
+        D;JLT
+
+    @LOOP
+    0;JMP
