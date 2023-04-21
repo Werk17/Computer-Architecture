532f406f8d2519c602578c3976dcf96ecc208d6a
Charles Werk <charles.werk@umconnect.umt.edu>
2023-04-21 08:40:11 -0600

    Some functions completed for compiler

---
+++ 10/CompileEngineStudent.py
@@ -0,0 +1,282 @@
+import sys
+from Tokenizer import Tokenizer
+
+
+class CompileEngine:
+    def __init__(self, T):
+        self.T = Tokenizer
+        self.currentToken = ""
+        self.currentTokenType = ""
+        # self.output = ""
+
+    # The following are suggested helper functions.
+    # You need not use them if you have other ideas
+    # about getting this done.
+
+    def process(self, str):
+        if self.currentToken == str:
+            self.printXMLToken()
+            self.T.advance()
+        else:
+            print("Syntax error")
+            self.T.advance()
+
+    def printXMLToken(self):
+        token = self.T.getToken()
+        type = self.T.getType()
+        print(f"<{type}> {token} </{type}>")
+
+    def checkToken(self, tokens):
+        """
+        Checks to see if the current token found in 
+        self.T.getToken()
+        is in the tuple tokens.
+        Print the token in appropriate XML if it is.
+        Give an error message and halt if it is not.
+        """
+        self.currentToken = self.T.getToken()
+        if type(tokens) is not tuple:
+            tokens = (tokens)
+        if(self.T.getToken()) in tokens:
+            self.process(self.T.getToken)
+        else:
+            print(f"Error: unexpected token {self.currentToken}")
+            exit()
+
+    def checkType(self, expected_types):
+        """
+        Checks to see if the current type found in 
+        self.T.getType()
+        is in the tuple expected types.
+        Print the token in appropriate XML and 
+        advance to the next token if it is.
+        Give an error message and halt if it is not.
+        """
+
+        if expected_types is not tuple:
+            expected_types = (expected_types)
+        if self.currentTokenType in expected_types:
+            self.process(self.T.getToken())
+        else:
+            print(
+                f"Error: expected types {expected_types} but got {self.currentTokenType}")
+            exit()
+
+    # The following are the compile functions that need to
+    # be implemented. compileExpression has been withheld for a
+    # future assignment
+
+    # def compile(self):
+    #     compiled_Code = ""
+    #     self.T.advance()
+
+    def compileClass(self):
+        """
+        Compiles <class> :=
+            'class' <class-name> '{' <class-var-dec>* <subroutine-dec>* '}'
+
+        The tokenizer is expected to be positionsed at the beginning of the
+        file.
+        """
+        print("<class>")
+        self.checkToken(("class",))
+        self.checkType(("IDENTIFIER",))
+        self.checkToken("{")
+        while self.T.getToken() in ["static", "field"]:
+            self.compileClassVarDec()
+        while self.T.getToken() in ["constructor", "function", "method"]:
+            self.compileSubroutine()
+        self.checkToken("}")
+        print("</class>")
+
+    def compileClassVarDec(self):
+        """
+        Compiles <class-var-dec> :=
+            ('static' | 'field') <type> <var-name> (',' <var-name>)* ';'
+        """
+        print("<classVarDec>")
+        self.checkToken(("static", "field"))
+        self.checkType((self.currentTokenType))
+        self.checkToken(self.currentToken)
+        while self.currentToken == ",":
+            self.checkToken((";"))
+        print("</classVarDec>")
+
+    def compileSubroutine(self):
+        """
+        Compiles <subroutine-dec> :=
+            ('constructor' | 'function' | 'method') ('void' | <type>)
+            <subroutine-name> '(' <parameter-list> ')' <subroutine-body>
+
+        ENTRY: Tokenizer positioned on the initial keyword.
+        EXIT:  Tokenizer positioned after <subroutine-body>.
+        """
+        print("<subroutineDec>")
+        self.checkToken(("constructor", "function", "method"))
+        if self.currentToken == "void":
+            self.checkToken(("void"))
+        else:
+            self.checkType((self.T.getType))
+        self.checkToken(self.T.getToken())  # compile subroutine name
+        self.checkToken(("("))
+        self.compileParameterList()
+        self.checkToken((")"))
+        self.compileSubroutineBody()
+        print("</subroutineDec>")
+
+    def compileParameterList(self):
+        """
+        Compiles <parameter-list> :=
+            ( <type> <var-name> (',' <type> <var-name>)* )?
+
+        ENTRY: Tokenizer positioned on the initial keyword.
+        EXIT:  Tokenizer positioned after <subroutine-body>.
+        """
+        print("<parameterList>")
+        if self.T.getToken() != ")":
+            self.checkType((self.T.getType()))
+            self.checkToken((self.T.getToken()))
+            while self.T.getToken == ",":
+                self.checkToken((","))
+                self.checkType(self.T.getType())
+                self.checkToken(self.T.getToken())
+        print("</parameterList>")
+
+    def compileSubroutineBody(self):
+        """
+        Compiles <subroutine-body> :=
+            '{' <var-dec>* <statements> '}'
+        """
+        print("<subroutineBody>")
+        self.process('{')
+        while self.T.getToken() in ('var'):
+            self.compileVarDec()
+        self.compileStatements()
+        self.process('}')
+        print("</subroutineBody>")
+
+    def compileVarDec(self):
+        """
+        Compiles <var-dec> :=
+            'var' <type> <var-name> (',' <var-name>)* ';'
+        """
+        print("<varDec>")
+        self.checkToken(("var"))
+        self.checkType((self.T.getType()))
+        self.checkToken((self.T.getToken()))
+        while self.T.getToken() == ',':
+            self.checkToken((","))
+            self.checkToken(self.T.getToken())
+        self.checkToken((";"))
+        print("</varDec>")
+
+    def compileStatements(self):
+        """
+        Compiles <statements> := (<let-statement> | <if-statement> |
+            <while-statement> | <do-statement> | <return-statement>)*
+        """
+        print("<statements>")
+        while self.T.getToken() in ('let', 'if', 'while', 'do', 'return'):
+            if self.T.getToken == 'let':
+                self.compileLet()
+            if self.T.getToken == 'if':
+                self.compileIf()
+            if self.T.getToken == 'while':
+                self.compileWhile()
+            if self.T.getToken == 'do':
+                self.compileDo()
+            if self.T.getToken == 'return':
+                self.compileReturn()
+        print("</statements>")
+
+    def compileLet(self):
+        """
+        Compiles <let-statement> :=
+            'let' <var-name> ('[' <expression> ']')? '=' <expression> ';'
+        """
+        print("<letStatement>")
+        print("</letStatement>")
+
+    def compileDo(self):
+        """
+        Compiles <do-statement> := 'do' <subroutine-call> ';'
+
+        <subroutine-call> := (<subroutine-name> '(' <expression-list> ')') |
+            ((<class-name> | <var-name>) '.' <subroutine-name> '('
+            <expression-list> ')')
+
+        <*-name> := <identifier>
+        """
+        print("<doStatement>")
+        print("</doStatment>")
+
+    def compileIf(self):
+        """
+        Compiles <if-statement> :=
+            'if' '(' <expression> ')' '{' <statements> '}' ( 'else'
+            '{' <statements> '}' )?
+        """
+        print("<ifStatement>")
+        print("</ifStatement>")
+
+    def compileWhile(self):
+        """
+        Compiles <while-statement> :=
+        'while' '(' <expression> ')' '{' <statements> '}'
+        """
+        print("<whileStatement>")
+
+        print("</whileStatement>")
+
+    def compileDo(self):
+        """
+        Compiles <do-statement> := 'do' <subroutine-call> ';'
+
+        <subroutine-call> := (<subroutine-name> '(' <expression-list> ')') |
+            ((<class-name> | <var-name>) '.' <subroutine-name> '('
+            <expression-list> ')')
+
+        <*-name> := <identifier>
+        """
+        print("<doStatement>")
+
+        print("</doStatement>")
+
+    def compileReturn(self):
+        """
+        Compiles <return-statement> :=
+            'return' <expression>? ';'
+        """
+        print("<returnStatement>")
+
+        print("</returnStatement>")
+
+
+# The following are not LL(1) and are not part of the initial assignment
+
+    def compileExpression(self):
+        """
+        Compiles <expression> :=
+            <term> (op <term>)*
+        """
+        print("<expression>")
+        print("</expression>")
+
+    def compileExpressionList(self):
+        """
+        Compiles <expression-list> :=
+            (<expression> (',' <expression>)* )?
+        """
+
+        print("<expressionList>")
+        print("</expressionList>")
+
+    def compileTerm(self):
+        """
+        Compiles a <term> :=
+            <int-const> | <string-const> | <keyword-const> | <var-name> |
+            (<var-name> '[' <expression> ']') | <subroutine-call> |
+            ( '(' <expression> ')' ) | (<unary-op> <term>)
+        """
+        print("<term>")
+        print("</term>")
--- 10/tokenizer.py
+++ 10/tokenizer.py
@@ -14,82 +14,141 @@ identStart = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
 identChars = identStart + numberChars
 
 
-def removeComments(f):
-    file = open(f, 'r')
-    jack_code = file.read()
+class Tokenizer:
 
-    # remove multline comments
-    jack_code = re.sub(r'/\*\*.*?\*/', '', jack_code)
-    # /\*\*  == /** (backslashes escape special meaning of *)
-    # .*?    == matches any character (the charaters in between the comment indicators
-    # \*/    == */
+    def __init__(self, infile):
+        infile = self.removeComments(infile)
+        infile = infile.strip()
+        self.code_lines = infile.splitlines()
+        self.tokens = []
+        self.keywords = keywords
+        self.symbols = symbols
+        self.numberChars = numberChars
+        self.numberStart = numberStart
+        self.identStart = identStart
+        self.identChars = identChars
+        self.curr_token = None
+        self.token_type = None
+        self.token_index = -1
+        self.tokenize()
 
-    # remove single line comments
-    jack_code = re.sub(r'//.*', '', jack_code)
+    def tokenize(self):
+        # tokens = []
+        for line in self.code_lines:
+            # OG: line_tokens = re.split(r'(\(|\)|,|;|\.|\s)', line)
+            line_tokens = re.split(r'(\(|\)|\[|\]|,|;|\.|\s|\".*?\")', line)
+            # (        \(  |  \)  |  ,  |  ;  |  \.  |  \s  |  \".*?\"        )
+            # \ is used to escape the special meaning of the character
+            #       - It is needed for the parentheses and the period becuase
+            #         they each have meaning within this re statement
+            #
+            # \( and \) take parentheses as tokens
+            # , ; and \. take commas, semicolons, and periods as tokens
+            # /s       takes spaces and whitesapce as tokens
+            # \".*?\"  takes everything inbetween double quotations and makes
+            #          it one big token including the quotations
 
-    file.close()
-    return jack_code
+            # Below prevents empty string tokens and whitespace tokens from being added to tokens list
+            for token in line_tokens:
+                if token != '' and not token.isspace():
+                    self.tokens.append(token)
+
+            # tokens is now a list of all the individual tokens we want to read
 
 
-def Main(f):
-    f = f.strip()
-    code_lines = f.splitlines()
+    def removeComments(self, f):
+        file = open(f, 'r')
+        jack_code = file.read()
 
-    tokens = []
-    for line in code_lines:
-        # OG: line_tokens = re.split(r'(\(|\)|,|;|\.|\s)', line)
-        line_tokens = re.split(r'(\(|\)|\[|\]|,|;|\.|\s|\".*?\")', line)
-        # (        \(  |  \)  |  ,  |  ;  |  \.  |  \s  |  \".*?\"        )
-        # \ is used to escape the special meaning of the character
-        #       - It is needed for the parentheses and the period becuase
-        #         they each have meaning within this re statement
-        #
-        # \( and \) take parentheses as tokens
-        # , ; and \. take commas, semicolons, and periods as tokens
-        # /s       takes spaces and whitesapce as tokens
-        # \".*?\"  takes everything inbetween double quotations and makes
-        #          it one big token including the quotations
+        # remove multline comments
+        jack_code = re.sub(r'/\*\*.*?\*/', '', jack_code)
+        # /\*\*  == /** (backslashes escape special meaning of *)
+        # .*?    == matches any character (the charaters in between the comment indicators
+        # \*/    == */
 
-        # Below prevents empty string tokens and whitespace tokens from being added to tokens list
-        for token in line_tokens:
-            if token != '' and not token.isspace():
-                tokens.append(token)
+        # remove single line comments
+        jack_code = re.sub(r'//.*', '', jack_code)
 
-    # tokens is now a list of all the individual tokens we want to read
+        file.close()
+        return jack_code
 
-    xml_code = '<tokens>\n'
-    for token in tokens:
+    def hasMoreTokens(self):
+        return self.token_index < len(self.tokens) - 1
 
-        if token in keywords:
-            xml_code += '<keyword> ' + str(token) + ' </keyword>\n'
-
-        elif token in symbols:
-            if token == '>':
-                xml_code += '<symbol> ' + '&gt;' + ' </symbol>\n'
-            elif token == '<':
-                xml_code += '<symbol> ' + '&lt;' + ' </symbol>\n'
-            elif token == '&':
-                xml_code += '<symbol> ' + '&amp;' + ' </symbol>\n'
+    def advance(self):
+        if self.hasMoreTokens():
+            self.token_index += 1
+            self.curr_token = self.tokens[self.token_index]
+            if self.curr_token in symbols:
+                self.token_type = "SYMBOL"
+            elif self.curr_token.isdigit():
+                self.token_type = "INT_CONST"
+            elif self.curr_token[0] == '"' and self.curr_token[-1] == '"':
+                self.token_type = "STRING_CONST"
+            elif self.curr_token in keywords:
+                self.token_type = "KEYWORD"
             else:
-                xml_code += '<symbol> ' + str(token) + ' </symbol>\n'
-
-        elif '"' in token:
-            token = token.replace('"', '')
-            xml_code += '<stringConstant> ' + str(token) + ' </stringConstant>\n'
-
-        elif token.isnumeric():
-            xml_code += '<integerConstant> ' + str(token) + ' </integerConstant>\n'
-
+                self.token_type = "IDENTIFIER"
         else:
-            xml_code += '<identifier> ' + str(token) + ' </identifier>\n'
-# end of file for xml codeF
-    xml_code += '</tokens>\n'
-    return xml_code
+            self.curr_token = ""
+            self.token_type = ""
+
+    def keyWords(self):
+        return self.keywords
+    
+    def getType(self):
+        return self.token_type
+
+    def getToken(self):
+        return self.curr_token
+    
+
 
 infile = sys.argv[1]
 xmlFile = infile.replace(".jack", "T.xml")
+tokenizer = Tokenizer(infile)
+# commentsRemoved = removeComments(infile)
+# outFile.close()
 
-commentsRemoved = removeComments(infile)
-f = open(xmlFile, 'a')
-f.write(Main(commentsRemoved))
-f.close()
+# code before compile engine came
+        # xml_code = '<tokens>\n'
+        # use while loop for has more tokens
+        # while(not self.hasMoreTokens()):
+        #     # advance to next token and do stuff
+        #     self.advance()
+
+        # for token in self.tokens:
+        #     self.curr_token = token
+
+        #     if token in keywords:
+        #         xml_code += '<keyword> ' + str(token) + ' </keyword>\n'
+        #         self.token_type = self.getType()
+
+        #     elif token in symbols:
+        #         self.token_type = self.getType()
+        #         if token == '>':
+        #             xml_code += '<symbol> ' + '&gt;' + ' </symbol>\n'
+        #         elif token == '<':
+        #             xml_code += '<symbol> ' + '&lt;' + ' </symbol>\n'
+        #         elif token == '&':
+        #             xml_code += '<symbol> ' + '&amp;' + ' </symbol>\n'
+        #         else:
+        #             xml_code += '<symbol> ' + str(token) + ' </symbol>\n'
+
+        #     elif '"' in token:
+        #         token = token.replace('"', '')
+        #         self.token_type = self.getType()
+        #         xml_code += '<stringConstant> ' + \
+        #             str(token) + ' </stringConstant>\n'
+
+        #     elif token.isnumeric():
+        #         self.token_type = self.getType()
+        #         xml_code += '<integerConstant> ' + \
+        #             str(token) + ' </integerConstant>\n'
+
+        #     else:
+        #         self.token_type = self.getToken()
+        #         xml_code += '<identifier> ' + str(token) + ' </identifier>\n'
+        # # end of file for xml codeF
+        # xml_code += '</tokens>\n'
+        # return xml_code
